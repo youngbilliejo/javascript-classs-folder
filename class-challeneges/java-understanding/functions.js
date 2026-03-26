c21f7355259e76bb42a4c8a02ae80d085f4add8b
Billie Young <youngbilliejo21@gmail.com>
2026-03-26 17:41:06 -0500

    funtion

---
+++ class-challeneges/java-understanding/functions.js
@@ -0,0 +1,17 @@
+function sayHello(num1){
+  console.log(num1 * 10);
+}
+
+for(let i = 0; i <5; i++){
+  sayHello(i);
+}
+
+for (let i = 10; i <15; i++){
+ sayHello (i); 
+}
+
+function tax(stateTax){
+  console.log(stateTax*0.07);
+}
+
+tax(5000);
